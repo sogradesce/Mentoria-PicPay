133bafb8c411ca39f65876906eb27ef902952e30
Bruno Valério de Oliveira <113361517+sogradesce@users.noreply.github.com>
2022-09-19 14:38:56 -0300

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('#t1').textContent = 'Java Script - Introdução';
-document.querySelector('#t2').textContent = 'Exemplificando algumas funcionalidades';
-
-const nome = prompt('Qual é o seu nome?');
-
-alert('Que bom ter vc aqui! \n Vamos começar? ');
