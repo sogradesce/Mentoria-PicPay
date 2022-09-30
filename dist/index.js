98dc4f8333853ac6b0d8c704a9a57ba8935cb5da
Francisco Bonomo Andrade <francisco.andrade@picpay.com>
2022-09-30 17:43:10 -0300

    ref: restore index.ts transpilation

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,3 @@
 "use strict";
-function printUser(usr) {
-    console.log(usr.name);
-    console.log(usr.age);
-    console.log(usr.password);
-}
-let usr1 = {
-    name: "Francisco",
-    age: 24,
-    password: "123321"
-};
-printUser(usr1);
+let x = "Chico feliz dev";
+console.log(x);
