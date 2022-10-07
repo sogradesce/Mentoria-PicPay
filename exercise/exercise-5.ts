20c3240ab00e7d1253983058fab3ff020c9d2641
Brunao <oliveiravr7@outlook.com>
2022-10-07 00:27:14 -0300

    Inicialização do Projeto v1

--- exercise/exercise-5.ts
+++ exercise/exercise-5.ts
@@ -1,101 +1,108 @@
-import {IsTypeEqual, IsTypeAssignable, Not, FirstArgument, SecondArgument, typeAssert} from 'type-assertions';
-import {logPerson, isUser, isAdmin, Person, persons, filterUsers} from './index';
+/*
 
-typeAssert<
-    IsTypeAssignable<
-        SecondArgument<typeof filterUsers>,
-        {name: string}
-    >
->();
-typeAssert<
-    IsTypeAssignable<
-        SecondArgument<typeof filterUsers>,
-        {age: number}
-    >
->();
-typeAssert<
-    IsTypeAssignable<
-        SecondArgument<typeof filterUsers>,
-        {name: string; age: number}
-    >
->();
-typeAssert<
-    IsTypeAssignable<
-        SecondArgument<typeof filterUsers>,
-        {occupation: string}
-    >
->();
-typeAssert<
-    IsTypeAssignable<
-        SecondArgument<typeof filterUsers>,
-        {name: string; age: number; occupation: string}
-    >
->();
-typeAssert<
-    Not<
-        IsTypeAssignable<
-            SecondArgument<typeof filterUsers>,
-            {hello: 'world'}
-        >
-    >
->();
-typeAssert<
-    IsTypeEqual<
-        ReturnType<typeof filterUsers>,
-        {type: 'user'; name: string; age: number; occupation: string}[]
-    >
->();
+Intro:
 
-typeAssert<
-    IsTypeEqual<
-        Person,
-        {name: string; age: number} & ({type: 'user'; occupation: string} | {type: 'admin'; role: string})
-    >
->();
+    Time to filter the data! In order to be flexible
+    we filter users using a number of criteria and
+    return only those matching all of the criteria.
+    We don't need Admins yet, we only filter Users.
 
-typeAssert<
-    IsTypeEqual<
-        typeof persons,
-        ({name: string; age: number} & ({type: 'user'; occupation: string} | {type: 'admin'; role: string}))[]
-    >
->();
+Exercise:
 
-typeAssert<
-    IsTypeEqual<
-        FirstArgument<typeof logPerson>,
-        {name: string; age: number} & ({type: 'user'; occupation: string} | {type: 'admin'; role: string})
-    >
->();
+    Without duplicating type structures, modify
+    filterUsers function definition so that we can
+    pass only those criteria which are needed,
+    and not the whole User information as it is
+    required now according to typing.
 
-typeAssert<
-    IsTypeEqual<
-        ReturnType<typeof logPerson>,
-        void
-    >
->();
+Higher difficulty bonus exercise:
 
-typeAssert<
-    IsTypeEqual<
-        FirstArgument<typeof isUser>,
-        {name: string; age: number} & ({type: 'user'; occupation: string} | {type: 'admin'; role: string})
-    >
->();
-typeAssert<
-    IsTypeEqual<
-        ReturnType<typeof isUser>,
-        boolean
-    >
->();
+    Exclude "type" from filter criterias.
 
-typeAssert<
-    IsTypeEqual<
-        FirstArgument<typeof isAdmin>,
-        {name: string; age: number} & ({type: 'user'; occupation: string} | {type: 'admin'; role: string})
-    >
->();
-typeAssert<
-    IsTypeEqual<
-        ReturnType<typeof isAdmin>,
-        boolean
-    >
->();
+*/
+
+interface User {
+    type: 'user';
+    name: string;
+    age: number;
+    occupation: string;
+}
+
+interface Admin {
+    type: 'admin';
+    name: string;
+    age: number;
+    role: string;
+}
+
+export type Person = User | Admin;
+
+export const persons: Person[] = [
+    { type: 'user', name: 'Max Mustermann', age: 25, occupation: 'Chimney sweep' },
+    {
+        type: 'admin',
+        name: 'Jane Doe',
+        age: 32,
+        role: 'Administrator'
+    },
+    {
+        type: 'user',
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    },
+    {
+        type: 'admin',
+        name: 'Bruce Willis',
+        age: 64,
+        role: 'World saver'
+    },
+    {
+        type: 'user',
+        name: 'Wilson',
+        age: 23,
+        occupation: 'Ball'
+    },
+    {
+        type: 'admin',
+        name: 'Agent Smith',
+        age: 23,
+        role: 'Administrator'
+    }
+];
+
+export const isAdmin = (person: Person): person is Admin => person.type === 'admin';
+export const isUser = (person: Person): person is User => person.type === 'user';
+
+export function logPerson(person: Person) {
+    let additionalInformation = '';
+    if (isAdmin(person)) {
+        additionalInformation = person.role;
+    }
+    if (isUser(person)) {
+        additionalInformation = person.occupation;
+    }
+    console.log(` - ${person.name}, ${person.age}, ${additionalInformation}`);
+}
+
+export function filterUsers(persons: Person[], criteria: Partial<Omit<User, 'type'>>): User[] {
+    return persons.filter(isUser).filter((user) => {
+        const criteriaKeys = Object.keys(criteria) as (keyof Omit<User, 'type'>)[];
+        return criteriaKeys.every((fieldName) => {
+            return user[fieldName] === criteria[fieldName];
+        });
+    });
+}
+
+console.log('Users of age 23:');
+
+filterUsers(
+    persons,
+    {
+        age: 23
+    }
+).forEach(logPerson);
+
+// In case if you are stuck:
+// https://www.typescriptlang.org/docs/handbook/utility-types.html
+// https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-8.html#predefined-conditional-types
