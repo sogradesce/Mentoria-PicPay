8db8f20ea97d9bf5d229dbce56375f9414e47464
Brunao <oliveiravr7@outlook.com>
2022-11-02 16:15:50 -0300

    Exercícios "Palindrome" e alguns exercícios do @leetcode
    Co-Authored-By: Francisco <48291373+franciscobonand@users.noreply.github.com>

---
+++ src/arrays/chicoExercicio_remove_duplicates_from_sorted_array.ts
@@ -0,0 +1,4 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+//sem tempo irmão, vou tentar fazer na quinta feira.
+//odeio windows.
--- src/strings/palindrome.ts
+++ src/strings/palindrome.ts
@@ -1,13 +1,32 @@
+import reverseStr from './revert';
 // Para testar: yarn test -- palindrome.test.ts
 
 // isPalindrome recebe uma string como entrada e deve retornar 'true'
 // se essa string for um palíndromo, e 'false' caso contrário.
 // Espaços em branco devem ser desconsiderados.
 function isPalindrome(s: string): boolean {
-    
-    return false;
-}
 
+    for(let i = 0; i < s.length / 2; i++) 
+    if (s[i] != s[s.length - i - 1]) 
+    if (s > " ") {
+        let chicoRepetida = ""
+        for (let i = 0; i < s.length; i++) {
+            chicoRepetida === chicoRepetida + s
+        }
+        //chicoRpetida >> better name
+    }
+    for (let i = s.length - 1; i >= 0; i--)
+
+    return true;
+    return false;
+    
+    }
+
+
+  // https://howtodoinjava.com/typescript/equals-vs-strict-equals/
+  // https://twitter.com/glaucia_lemos86/status/1362153943393071106
+
+    
 // Dois jeitos diferentes de fazer essa função:
 // 1º: Pega a string "s", usa a função reverse e salva o resultado em uma string "j", e compra "s" com "j"
 // 2º: Faz um loop que vai até o s.length/2, a cada passo vc compara a letra s[i] com a letra do lugar s[length - i - 1]
