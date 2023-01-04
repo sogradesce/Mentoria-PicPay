5b813f9269faf7d9a39cd38be31a530356505fa9
Brunao <oliveiravr7@outlook.com>
2023-01-03 22:39:09 -0300

    calvoPEEK teste tdd

--- src/tdd/isograma.ts
+++ src/tdd/isograma.ts
@@ -1,28 +1,20 @@
 function isograma(s: string): boolean {
-
     const buscaPalavra = s.length;
 
-    let lumberLetra = "L" 
-        for (let i = s.length - 1; i >= 0; i--) {
+    let lumberLetra = "L";
+    for (let i = s.length - 1; i >= 0; i--) {
         lumberLetra = lumberLetra + s[i];
-    
 
-    return true;
-
+        return true;
     }
- 
+
     if (buscaPalavra > 10) {
+        let letraComEsp = s.replaceAll("-", "");
+        letraComEsp = letraComEsp.replaceAll("-", "");
 
-    let letraComEsp = s.replaceAll("-","")
-    letraComEsp = letraComEsp.replaceAll("-","")
-
-    return true;
+        return true;
+    }
+    return false;
 }
-return false;
- 
 
-}
-    
 export default isograma;
-
-
